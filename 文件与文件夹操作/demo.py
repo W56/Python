4492f212a44968950e9e98ef096a63aae1aa3c3f
SH <39229808+0215Wyz@users.noreply.github.com>
2019-04-26 10:24:03 +0800

    Add files via upload

---
+++ 文件与文件夹操作/demo.py
@@ -0,0 +1,5 @@
+Hello world1
+Hello world2
+Hello world3
+Hello world4
+Hello world5
---
+++ 文件与文件夹操作/demo_new.py
@@ -0,0 +1,5 @@
+Hello world1                 #0
+Hello world2                 #1
+Hello world3                 #2
+Hello world4                 #3
+Hello world5                 #4
